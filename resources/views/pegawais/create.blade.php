6ce4aa3df5f923bfeb3a3fa95a494357d01bc6c0
agungbudisan <agungbudisantoso@student.uns.ac.id>
2024-06-28 17:59:20 +0700

    Remove unnecessary script tags in create.blade.php

--- resources/views/pegawais/create.blade.php
+++ resources/views/pegawais/create.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    @vite('resources/css/app.css', 'resources/js/app.js')
     <title>Tambah Pegawai Baru</title>
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
@@ -34,7 +33,5 @@
 
         <a href="{{ url('/') }}" class="mt-4 inline-block text-blue-500 hover:underline">Kembali ke Daftar</a>
     </div>
-
-    <script src="./node_modules/preline/dist/preline.js"></script>
 </body>
 </html>
